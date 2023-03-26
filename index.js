2846f3c4611ffd584e45c2ae5c6a6c39e1bd2db3
reaganmunene <reagannmunene@gmail.com>
2023-03-26 19:47:54 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,25 @@
+function add(x,y){
+    return x+y;
+}
+function subtract(x,y){
+    return x-y;
+}
+function divide(a,b){
+    return a/b;
+}
+function multiply(a,b){
+    return a*b;
+}
+let number
+function add5(){
+    return (number+= 5);
+}
+function add5(){
+    return (number-= 50);
+}
+function makeInt(string){
+    parseInt("nonsense!", 10);
+}
+function preserveDecimal(string){
+    parseFloat("80.123999");;
+}
